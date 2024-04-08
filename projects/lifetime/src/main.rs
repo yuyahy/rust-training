d2d5f285f666b1c5cef5b9c55dbd4080768dc4c3
yuyahy <xvto.ya@gmail.com>
2024-04-08 12:49:40 +0000

    feat: lifetimeのサンプルコードを追加

---
+++ projects/lifetime/src/main.rs
@@ -0,0 +1,16 @@
+// 引数によって、x or yのどちらの参照を返却するかが変わるため、
+// コンパイラは所有権の生存期間(ライフタイム)が分からない
+fn longest(x: &str, y: &str) -> &str {
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
+}
+fn main() {
+    let string1 = String::from("abcd");
+    let string2 = "xyz";
+
+    let result = longest(string1.as_str(), string2);
+    println!("The longest string is {}", result);
+}
